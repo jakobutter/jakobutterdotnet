1cfab2801bbc78747f7383201bc180a1f7be9188
jakobutter <88466876+jakobutter@users.noreply.github.com>
2024-07-11 21:20:13 -0500

    Create musicscript.js

---
+++ scripts/musicscript.js
@@ -0,0 +1,65 @@
+var songIndex = 0;
+var audioPlayer = document.getElementById('player');
+var songList = document.getElementById('songList');
+var playPauseButton = document.getElementById('playPauseButton');
+var progressBar = document.getElementById('progressBar');
+
+audioPlayer.onended = function() {
+   nextSong();
+};
+
+audioPlayer.ontimeupdate = function() {
+   var progress = audioPlayer.currentTime / audioPlayer.duration * 100 + '%';
+   progressBar.children[0].style.width = progress; 
+};
+
+function playPause() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        playPauseButton.innerText = 'I I';
+    } else {
+        audioPlayer.pause();
+        playPauseButton.innerText = '>';
+    }
+}
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    playSong();
+}
+
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    playSong();
+}
+
+function playSong() {
+    audioPlayer.src = songs[songIndex].file;
+    audioPlayer.play();
+    playPauseButton.innerText = 'I I';
+    updateSongList();
+}
+
+function updateSongList() {
+   songList.innerHTML = songs.map(function(song, index) {
+       return '<div class="song' + (index === songIndex ? ' playing' : '') + '" onclick="selectSong(' + index + ')">' + song.name + '</div>';
+   }).join('');
+}
+
+function selectSong(index) {
+   songIndex = index;
+   playSong();
+}
+
+function scrub(event) {
+   var x = event.offsetX / progressBar.offsetWidth * audioPlayer.duration; 
+   audioPlayer.currentTime = x; 
+}
+
+updateSongList();
